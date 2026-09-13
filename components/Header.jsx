0301517d475e6f8399c0bb081a202fe606f7a86a
Yashpal Singh <yashsinghh0777@gmail.com>
2026-09-13 11:22:13 +0530

    create header component

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Text } from 'ink';
+
+export const Header = () => {
+    return (
+        <Box justifyContent="center" marginBottom={1}>
+            <Text bold color="blueBright">🎶 Terminal Rhythm</Text>
+        </Box>
+    );
+};
