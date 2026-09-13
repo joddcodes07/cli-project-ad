083778df8d469a805b23a7e487011626e5962df8
Yashpal Singh <yashsinghh0777@gmail.com>
2026-09-13 11:23:18 +0530

    create NowPlaying component structure

---
+++ components/NowPlaying.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Text } from 'ink';
+
+export const NowPlaying = ({ currentPlaying }) => {
+    const displayName = currentPlaying ? currentPlaying.replace(/\.[^/.]+$/, "") : "None";
+
+    return (
+        <Box flexDirection="column" width="50%" borderStyle="singleLeft" borderColor="gray" paddingLeft={2}>
+            <Text bold color="magenta" marginBottom={1}>Now Playing</Text>
+            
+            <Box flexDirection="column" marginBottom={1}>
+                <Text color="gray">Track:</Text>
+                <Text color="white">{displayName}</Text>
+            </Box>
+
+            <Box flexDirection="column">
+                <Text color="gray">Status:</Text>
+                <Text color={currentPlaying ? "green" : "dim"}>
+                    {currentPlaying ? "Playing" : "Stopped"}
+                </Text>
+            </Box>
+        </Box>
+    );
+};
