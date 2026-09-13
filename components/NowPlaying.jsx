19e6e40d938a138a5a9df1755e2a0177e9daf93d
Yashpal Singh <yashsinghh0777@gmail.com>
2026-09-13 11:42:29 +0530

    render dynamic ASCII album art placeholder

--- components/NowPlaying.jsx
+++ components/NowPlaying.jsx
@@ -8,15 +8,36 @@ export const NowPlaying = ({ currentPlaying, trackInfo, isPaused }) => {
     const artist = trackInfo ? trackInfo.artist : "Unknown Artist";
     const album = trackInfo ? trackInfo.album : "Unknown Album";
 
+    // Dynamic ASCII Art based on playback state
+    const asciiArt = currentPlaying ? [
+        " 💽  ██████ ",
+        "    ██████ ",
+        "    ██████ "
+    ] : [
+        " 📭  ░░░░░░ ",
+        "    ░░░░░░ ",
+        "    ░░░░░░ "
+    ];
+
     return (
         <Box flexDirection="column" width="50%" paddingLeft={2}>
             <Text bold color="magenta" marginBottom={1}>Now Playing</Text>
             
-            <Box flexDirection="column" marginBottom={1}>
-                <Text color="gray">Track:</Text>
-                <Text color="white" bold>{title}</Text>
-                <Text color="cyan">{artist}</Text>
-                <Text color="dim">{album}</Text>
+            <Box flexDirection="row" marginBottom={1}>
+                {/* ASCII Art Box */}
+                <Box flexDirection="column" marginRight={3}>
+                    {asciiArt.map((line, i) => (
+                        <Text key={i} color={currentPlaying ? "cyan" : "gray"}>{line}</Text>
+                    ))}
+                </Box>
+                
+                {/* Metadata Box */}
+                <Box flexDirection="column">
+                    <Text color="gray">Track:</Text>
+                    <Text color="white" bold>{title}</Text>
+                    <Text color="cyan">{artist}</Text>
+                    <Text color="dim">{album}</Text>
+                </Box>
             </Box>
 
             <Box flexDirection="column">
